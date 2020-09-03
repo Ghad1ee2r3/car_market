4cf86af7c97c5fd41781b195624e70e095aad080
ghadeer <apps02977@gmail.com>
2020-09-03 22:07:31 +0300

    task car

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -19,14 +21,49 @@ def car_detail(request, car_id):
 
 def car_create(request):
 	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST , request.FILES)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ' add  success.')
+			return redirect('car-list')
+	context = {
+			"form":form,
+			}
+	#messages.success(request, 'create car success.')
+	return render(request, 'create.html', context)
+
+
 
 
 def car_update(request, car_id):
 	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST,request.FILES, instance=car_obj)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Your car was updated success.')
+			return redirect('car-list')
+	context = {
+			"car_obj": car_obj,
+			"form":form,
+			}
+	#messages.set_level(request, messages.WARNING)
+
+	#CRITICAL = 50
+	#messages.add_message(request, CRITICAL, 'A serious error occurred.')
+	return render(request, 'update.html', context)
+
+
 
 
 def car_delete(request, car_id):
 	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	car_obj.delete()
+	messages.success(request, ' delete  success.')
+	return redirect('car-list')
+	#return render(...)
